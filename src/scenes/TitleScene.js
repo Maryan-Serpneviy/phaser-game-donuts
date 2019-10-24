34117c74e5f39cf43fb42a94e7cee7b58339b053
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-10-24 15:04:16 +0300

    Title scene added

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -1,5 +1,7 @@
 import Phaser from 'phaser';
 import Image from '../scripts/images';
+import Const from '../scripts/constants';
+import '../assets/fonts/fonts.css';
 
 export default class TitleScene extends Phaser.Scene {
     constructor() {
@@ -23,32 +25,30 @@ export default class TitleScene extends Phaser.Scene {
         bg.displayWidth = window.innerWidth;
         bg.displayHeight = window.innerHeight;
 
-        const LOGO_PAD_X = 0.05;
-        const LOGO_PAD_Y = 0.05;
-        const LOGO_SCALE = 1.45;
-        const logo = this.add.image(SCREEN_WIDTH * LOGO_PAD_X, SCREEN_HEIGHT * LOGO_PAD_Y, 'logo')
-            .setOrigin(0, 0).setScale(LOGO_SCALE)
-        const logoOffsetY = logo.height * LOGO_SCALE + logo.y
+        const logo = this.add.image(null, SCREEN_HEIGHT * Const.TITLE.LOGO_PAD_Y, 'logo')
+            .setOrigin(0, 0).setScale(Const.TITLE.SCALE.LOGO);
+        logo.x = SCREEN_WIDTH / 2 - this.children.list[1].width * Const.TITLE.SCALE.LOGO / 2;
+        const logoOffsetY = logo.height * Const.TITLE.SCALE.LOGO + logo.y
         
-        const DONUT_PAD = 0.125;
-        const DONUT_SCALE = 1.25;
-        const donutShadow = this.add.image(SCREEN_WIDTH * DONUT_PAD, logoOffsetY + 20, 'big-shadow')
-            .setOrigin(0, 0).setScale(DONUT_SCALE);
+        const donutShadow = this.add.image(null, logoOffsetY + 20, 'big-shadow')
+            .setOrigin(0, 0).setScale(Const.TITLE.SCALE.DONUT);
+        donutShadow.x = SCREEN_WIDTH / 2 - this.children.list[2].width * Const.TITLE.SCALE.DONUT / 2;
 
         const donut = this.add.image(donutShadow.x, donutShadow.y, 'donut')
-            .setOrigin(0, 0).setScale(DONUT_SCALE);
-        const donutOffsetY = donut.height * DONUT_SCALE + donut.y;
-        console.log(logoOffsetY, donutOffsetY)
-        const PLAY_SCALE = 2;
-        const play = this.add.image(SCREEN_WIDTH / 2, donutOffsetY + 230, 'btn-play')
-            .setScale(PLAY_SCALE);
-        
-        const SFX_SCALE = 0.8;
-        const sfx = this.add.image(SCREEN_WIDTH - 150, SCREEN_HEIGHT - 150, 'btn-sfx')
-            .setOrigin(0, 0).setScale(SFX_SCALE);
+            .setOrigin(0, 0).setScale(Const.TITLE.SCALE.DONUT);
+        const donutOffsetY = donut.height * Const.TITLE.SCALE.DONUT + donut.y;
 
-        const TUTORIAL_SCALE = 5;
-        const tutorial = this.add.text(SCREEN_WIDTH / 5, SCREEN_HEIGHT - 125, 'HOW TO PLAY', { fontFamily: 'Fredoka One' })
-            .setScale(TUTORIAL_SCALE)
+        this.add.image(SCREEN_WIDTH / 2, donutOffsetY + 230, 'btn-play').setScale(Const.TITLE.SCALE.PLAY);
+        
+        this.add.image(SCREEN_WIDTH - 130, SCREEN_HEIGHT - 150, 'btn-sfx')
+            .setOrigin(0, 0).setScale(Const.TITLE.SCALE.SFX);
+
+        createText(this, SCREEN_WIDTH / 5, SCREEN_HEIGHT - 125, 'HOW TO PLAY', 'cursive', '80px');
     }
+}
+
+function createText(ctx, x, y, text, family, size) {
+    const t = ctx.add.text(x, y, text, { fontFamily: family, fontSize: size });
+    t.setShadow(5, 5, 'rgba(0,0,0,0.5)', 15, true);
+    return t;
 }
---
+++ src/scripts/constants.js
@@ -0,0 +1,11 @@
+export default {
+    TITLE: {
+        LOGO_PAD_Y: 0.05,
+        SCALE: {
+            LOGO: 1.45,
+            DONUT: 1.25,
+            PLAY: 2,
+            SFX: 0.8
+        },
+    }
+}
